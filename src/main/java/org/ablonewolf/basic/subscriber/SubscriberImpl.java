d312b2913b6cae4daf9dd4937a2e63cba8703fac
Arka Bhuiyan <belmont66@duck.com>
2024-07-16 16:29:21 +0600

    created an implementation of Subscriber.

---
+++ src/main/java/org/ablonewolf/basic/subscriber/SubscriberImpl.java
@@ -0,0 +1,34 @@
+package org.ablonewolf.basic.subscriber;
+
+import lombok.Getter;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Getter
+public class SubscriberImpl implements Subscriber<String> {
+
+    public static final Logger logger = LoggerFactory.getLogger(SubscriberImpl.class);
+    private Subscription subscription;
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        this.subscription = subscription;
+    }
+
+    @Override
+    public void onNext(String s) {
+        logger.info("received: {}", s);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        logger.error("error", throwable);
+    }
+
+    @Override
+    public void onComplete() {
+        logger.info("Completed");
+    }
+}
