559ccdd3723d0636d71f168a0992e7cf5cd1dc59
Arka Bhuiyan <belmont66@duck.com>
2024-07-25 09:23:57 +0600

    created an implementation of Publisher and passed the subscriber object to the subscription using the constructor in the SubscriptionImpl class.

---
+++ src/main/java/org/ablonewolf/basic/publisher/PublisherImpl.java
@@ -0,0 +1,15 @@
+package org.ablonewolf.basic.publisher;
+
+import org.ablonewolf.basic.subscription.SubscriptionImpl;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+public class PublisherImpl implements Publisher<String> {
+
+    @Override
+    public void subscribe(Subscriber<? super String> subscriber) {
+        Subscription subscription = new SubscriptionImpl(subscriber);
+        subscriber.onSubscribe(subscription);
+    }
+}
--- src/main/java/org/ablonewolf/basic/subscription/SubscriptionImpl.java
+++ src/main/java/org/ablonewolf/basic/subscription/SubscriptionImpl.java
@@ -1,12 +1,16 @@
 package org.ablonewolf.basic.subscription;
 
+import lombok.AllArgsConstructor;
+import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@AllArgsConstructor
 public class SubscriptionImpl implements Subscription {
 
     public static final Logger logger = LoggerFactory.getLogger(SubscriptionImpl.class);
+    private Subscriber<? super String> subscriber;
 
     @Override
     public void request(long l) {
