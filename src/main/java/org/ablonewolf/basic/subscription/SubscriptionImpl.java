56f85538dbcd8be09d7eacfca7d6598798c10c0f
Arka Bhuiyan <belmont66@duck.com>
2024-07-26 04:52:11 +0600

    cleaned up the code in the method onRequest of SubscriptionImpl

--- src/main/java/org/ablonewolf/basic/subscription/SubscriptionImpl.java
+++ src/main/java/org/ablonewolf/basic/subscription/SubscriptionImpl.java
@@ -25,35 +25,33 @@ public class SubscriptionImpl implements Subscription {
             return;
         }
 
-        if (requested > MAX_ITEMS) {
-            this.subscriber.onError(new RuntimeException("Number of requested items exceeds maximum allowed."));
-            this.isCancelled = true;
+        if (requested > 0 && requested <= MAX_ITEMS) {
+            logger.info("Subscriber has requested {} items", requested);
+            for (int index = 0; index < requested && count < MAX_ITEMS; index++) {
+                this.subscriber.onNext(this.faker.internet().emailAddress());
+                ++count;
+            }
+
+            if (Objects.equals(count, MAX_ITEMS)) {
+                logger.info("No more email address to produce");
+                this.subscriber.onComplete();
+                this.isCancelled = true;
+            }
+        } else {
+            if (requested > MAX_ITEMS) {
+                this.subscriber.onError(new RuntimeException("Number of requested items exceeds maximum allowed."));
+                this.isCancelled = true;
+            } else if (requested < 0) {
+                this.subscriber.onError(new RuntimeException("Number of requested items cannot be negative."));
+                this.isCancelled = true;
+            } else {
+                logger.info("Subscriber has asked for zero items. Therefore completing the subscription");
+                this.subscriber.onComplete();
+                this.isCancelled = true;
+            }
             return;
         }
 
-        if (requested < 0) {
-            this.subscriber.onError(new RuntimeException("Number of requested items cannot be negative."));
-            this.isCancelled = true;
-            return;
-        }
-
-        if (requested == 0) {
-            logger.info("Subscriber has asked for zero items. Therefore completing the subscription");
-            this.subscriber.onComplete();
-            this.isCancelled = true;
-        }
-
-        logger.info("Subscriber has requested {} items", requested);
-        for (int index = 0; index < requested && count < MAX_ITEMS; index++) {
-            this.subscriber.onNext(this.faker.internet().emailAddress());
-            ++count;
-        }
-
-        if (Objects.equals(count, MAX_ITEMS)) {
-            logger.info("No more email address to produce");
-            this.subscriber.onComplete();
-            this.isCancelled = true;
-        }
     }
 
     @Override
