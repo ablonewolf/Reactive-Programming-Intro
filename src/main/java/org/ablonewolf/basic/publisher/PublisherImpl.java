cfb3be90f6b49629713ee86c929d36fc45ef6ec1
Arka Bhuiyan <belmont66@duck.com>
2024-07-26 03:07:16 +0600

    Added a faker instance in the Subscription to generate fake email addresses whenever the subscriber requests for a new email address and added count and max count variables so that email generation will occur for certain number of periods.

--- src/main/java/org/ablonewolf/basic/publisher/PublisherImpl.java
+++ src/main/java/org/ablonewolf/basic/publisher/PublisherImpl.java
@@ -1,5 +1,6 @@
 package org.ablonewolf.basic.publisher;
 
+import com.github.javafaker.Faker;
 import org.ablonewolf.basic.subscription.SubscriptionImpl;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
@@ -9,7 +10,7 @@ public class PublisherImpl implements Publisher<String> {
 
     @Override
     public void subscribe(Subscriber<? super String> subscriber) {
-        Subscription subscription = new SubscriptionImpl(subscriber);
+        Subscription subscription = new SubscriptionImpl(subscriber, Faker.instance());
         subscriber.onSubscribe(subscription);
     }
 }
--- src/main/java/org/ablonewolf/basic/subscription/SubscriptionImpl.java
+++ src/main/java/org/ablonewolf/basic/subscription/SubscriptionImpl.java
@@ -1,24 +1,45 @@
 package org.ablonewolf.basic.subscription;
 
-import lombok.AllArgsConstructor;
+import com.github.javafaker.Faker;
+import lombok.RequiredArgsConstructor;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@AllArgsConstructor
+import java.util.Objects;
+
+@RequiredArgsConstructor
 public class SubscriptionImpl implements Subscription {
 
     public static final Logger logger = LoggerFactory.getLogger(SubscriptionImpl.class);
-    private Subscriber<? super String> subscriber;
+    public static final Integer MAX_ITEMS = 10;
+    private final Subscriber<? super String> subscriber;
+    private final Faker faker;
+    private Boolean isCancelled;
+    private Integer count = 0;
 
     @Override
-    public void request(long l) {
+    public void request(long requested) {
+        if (isCancelled) {
+            return;
+        }
+        logger.info("Subscriber has requested {} items", requested);
+        for (int index = 0; index < requested && count < MAX_ITEMS; index++) {
+            this.subscriber.onNext(this.faker.internet().emailAddress());
+            ++count;
+        }
 
+        if (Objects.equals(count, MAX_ITEMS)) {
+            logger.info("No more email address to produce");
+            this.subscriber.onComplete();
+            this.isCancelled = true;
+        }
     }
 
     @Override
     public void cancel() {
-
+        logger.info("Subscriber has cancelled");
+        this.isCancelled = true;
     }
 }
