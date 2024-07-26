27f8bfb1d7dd40aa44cd81aa0b6e4913880af7cb
Arka Bhuiyan <belmont66@duck.com>
2024-07-26 09:29:04 +0600

    added missing javadoc comment for the method.

--- src/main/java/org/ablonewolf/common/Util.java
+++ src/main/java/org/ablonewolf/common/Util.java
@@ -5,6 +5,12 @@ import reactor.core.publisher.Mono;
 
 public class Util {
 
+    /**
+     * method for returning a new instance of default subscriber
+     *
+     * @param name: the name of the subscriber assigned by the publisher
+     * @return default subscriber
+     */
     public static <T> Subscriber<T> subscriber(String name) {
         return new DefaultSubscriber<>(name);
     }
