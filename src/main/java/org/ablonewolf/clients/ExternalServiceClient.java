5a734c11edf5642099803e37650eae4636006f67
Arka Bhuiyan <belmont66@duck.com>
2024-07-29 05:07:59 +0600

    added a http client for external service to consume APIs from external service and added the method to consume the API of getting product name via the product ID.

---
+++ src/main/java/org/ablonewolf/clients/ExternalServiceClient.java
@@ -0,0 +1,18 @@
+package org.ablonewolf.clients;
+
+import org.ablonewolf.common.AbstractHttpClient;
+import reactor.core.publisher.Mono;
+
+/**
+ * an external service client to communicate with the external service and consume its APIs
+ */
+public class ExternalServiceClient extends AbstractHttpClient {
+
+    public Mono<String> getProductName(Integer productId) {
+        return this.httpClient.get()
+                .uri(String.format("/demo01/product/%d", productId))
+                .responseContent()
+                .asString()
+                .next();
+    }
+}
