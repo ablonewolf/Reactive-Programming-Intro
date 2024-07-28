35985cf4d4e4ee4d68b351b69094b5d00d6c0ec7
Arka Bhuiyan <belmont66@duck.com>
2024-07-29 05:06:01 +0600

    added missing javadoc comment.

--- src/main/java/org/ablonewolf/common/AbstractHttpClient.java
+++ src/main/java/org/ablonewolf/common/AbstractHttpClient.java
@@ -3,6 +3,9 @@ package org.ablonewolf.common;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.LoopResources;
 
+/**
+ * an abstract http client to consume APIs from foreign services
+ */
 public abstract class AbstractHttpClient {
 
     private static final String BASE_URL = "https://localhost:7070/";
