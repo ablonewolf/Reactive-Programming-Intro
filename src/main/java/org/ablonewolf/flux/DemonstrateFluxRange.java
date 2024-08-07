50ebff015943dcf15fbd89d956ed41d58fc873b1
Arka Bhuiyan <belmont66@duck.com>
2024-08-07 07:26:44 +0600

    demonstrated how to use the Flux.range function.

---
+++ src/main/java/org/ablonewolf/flux/DemonstrateFluxRange.java
@@ -0,0 +1,22 @@
+package org.ablonewolf.flux;
+
+import org.ablonewolf.common.Util;
+import reactor.core.publisher.Flux;
+
+/**
+ * demonstrated how to use the Flux range function
+ */
+public class DemonstrateFluxRange {
+    public static void main(String[] args) {
+        // this will create a flux of numbers from 3 to 12; the first param is starting point, the next param is the
+        // count
+        var fluxRange = Flux.range(3, 10);
+
+        fluxRange.subscribe(Util.subscriber("Integer Subscriber"));
+
+        // generating 10 random names
+        Flux.range(1, 10)
+                .map(index -> Util.getFaker().name().firstName())
+                .subscribe(Util.subscriber("FirstName Subscriber"));
+    }
+}
