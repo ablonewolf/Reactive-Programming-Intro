787d3fc7dcc7b918afaab9e33ca720507850de49
Arka Bhuiyan <belmont66@duck.com>
2025-05-11 09:34:25 +0600

    logged the duration to see how much time it takes to complete fetching products.

--- src/main/java/org/ablonewolf/mono/DemonstrateNonBlockingIO.java
+++ src/main/java/org/ablonewolf/mono/DemonstrateNonBlockingIO.java
@@ -5,26 +5,32 @@ import org.ablonewolf.common.Util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 /**
  * demonstrated the non-blocking IO operation using the external service client
  */
 public class DemonstrateNonBlockingIO {
 
-    private static final Logger logger = LoggerFactory.getLogger(DemonstrateNonBlockingIO.class);
+	private static final Logger logger = LoggerFactory.getLogger(DemonstrateNonBlockingIO.class);
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        var client = new ExternalServiceClient();
+		var client = new ExternalServiceClient();
 
-        logger.info("Starting the Non-BlockingIO operation.");
+		logger.info("Starting the Non-BlockingIO operation.");
+		var startTime = LocalDateTime.now();
 
-        for (int index = 1; index <= 20; index++) {
-            client.getProductName(index)
-                    .subscribe(Util.subscriber("Product Consuming subscriber"));
-        }
+		for (int index = 1; index <= 100; index++) {
+			client.getProductName(index)
+					.subscribe(Util.subscriber("Product Consuming subscriber"));
+		}
+		var endTime = LocalDateTime.now();
 
-        Util.sleepSeconds(2L);
-
-        logger.info("Completed the Non-BlockingIO operation.");
-    }
+		Util.sleepSeconds(2L);
+		logger.info("Time taken to complete the IO operation: {} milliseconds",
+				Duration.between(startTime, endTime).toMillis());
+		logger.info("Completed the Non-BlockingIO operation.");
+	}
 }
