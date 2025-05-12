7aebacbc2de65d89112e16d638533ec59e934162
Arka Bhuiyan <belmont66@duck.com>
2025-05-12 12:14:24 +0600

    demonstrated how to consume a flux of items from an external service

---
+++ src/main/java/org/ablonewolf/flux/DemonstrateNonBlockingIO.java
@@ -0,0 +1,20 @@
+package org.ablonewolf.flux;
+
+import org.ablonewolf.common.Util;
+import org.ablonewolf.services.impl.ExternalServiceClient;
+
+/**
+ * demonstrated how to consume a flux of items from an external service
+ */
+public class DemonstrateNonBlockingIO {
+
+	public static void main(String[] args) {
+
+		var client = new ExternalServiceClient();
+
+		client.getStreamOfNames()
+				.subscribe(Util.subscriber("Random Name Subscriber"));
+
+		Util.sleepSeconds(8L);
+	}
+}
