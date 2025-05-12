c200cd72fb9041622fae82bdc136106a9f824df2
Arka Bhuiyan <belmont66@duck.com>
2025-05-12 15:40:02 +0600

    demonstrated how to create an empty flux and a flux with a custom error

---
+++ src/main/java/org/ablonewolf/flux/DemonstrateFluxEmptyAndError.java
@@ -0,0 +1,21 @@
+package org.ablonewolf.flux;
+
+import org.ablonewolf.common.Util;
+import reactor.core.publisher.Flux;
+
+/**
+ * demonstrating how to create an empty flux and a flux with a custom error
+ */
+public class DemonstrateFluxEmptyAndError {
+
+	public static void main(String[] args) {
+
+		Flux.empty()
+				.subscribe(Util.subscriber("Empty Subscriber"));
+
+		Flux.error(new RuntimeException("Custom Error"))
+				.subscribe(Util.subscriber("Sample Subscriber"));
+
+		Util.sleepSeconds(2L);
+	}
+}
