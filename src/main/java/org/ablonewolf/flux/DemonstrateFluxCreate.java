c3bcdf1639770ee8e98676301b9cb91da2e0acc4
Arka Bhuiyan <belmont66@duck.com>
2025-05-13 19:33:38 +0600

    refactored the process of name generator to another consumer to make the code more maintainable.

--- src/main/java/org/ablonewolf/flux/DemonstrateFluxCreate.java
+++ src/main/java/org/ablonewolf/flux/DemonstrateFluxCreate.java
@@ -2,19 +2,39 @@ package org.ablonewolf.flux;
 
 import org.ablonewolf.common.Util;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+
+import java.util.function.Consumer;
 
 public class DemonstrateFluxCreate {
 
 	public static void main(String[] args) {
 
-		Flux.create(fluxSink -> {
-					String country;
-					do {
-						country = Util.getFaker().country().name();
-						fluxSink.next(country);
-					} while (!country.equalsIgnoreCase("canada"));
-					fluxSink.complete();
-				})
-				.subscribe(Util.subscriber("Country Subscriber"));
+		var countryNameGenerator = new CountryNameGenerator();
+		var countryFlux = Flux.create(countryNameGenerator);
+
+		countryFlux.subscribe(Util.subscriber("Country Subscriber"));
+
+		countryNameGenerator.generate();
+	}
+
+	private static class CountryNameGenerator implements Consumer<FluxSink<String>> {
+
+		private FluxSink<String> fluxSink;
+
+		@Override
+		public void accept(FluxSink<String> fluxSink) {
+			this.fluxSink = fluxSink;
+		}
+
+		public void generate() {
+			String countryName;
+			do {
+				countryName = Util.getFaker().country().name();
+				this.fluxSink.next(countryName);
+			} while (!countryName.equalsIgnoreCase("canada"));
+
+			this.fluxSink.complete();
+		}
 	}
 }
