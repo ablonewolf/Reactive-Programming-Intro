4374bf8283065534f63ab929d347613cade3b1bb
Arka Bhuiyan <belmont66@duck.com>
2025-05-13 11:52:31 +0600

    demonstrated the stock purchasing operation using the remote stock service and the stock price observer.

---
+++ src/main/java/org/ablonewolf/flux/DemonstrateStockPriceChange.java
@@ -0,0 +1,24 @@
+package org.ablonewolf.flux;
+
+import org.ablonewolf.basic.subscriber.StockPriceObserver;
+import org.ablonewolf.common.Util;
+import org.ablonewolf.services.impl.ExternalServiceClient;
+
+/**
+ * demonstrating how we can subscribe to a sample stock price change on a remote service
+ * to make this work, we have to run the external service located inside the 'resources' directory.
+ */
+public class DemonstrateStockPriceChange {
+
+	public static void main(String[] args) {
+
+		var client = new ExternalServiceClient();
+
+		var stockPriceObserver = new StockPriceObserver();
+
+		client.getPriceChanges()
+				.subscribe(stockPriceObserver);
+
+		Util.sleepSeconds(20L);
+	}
+}
