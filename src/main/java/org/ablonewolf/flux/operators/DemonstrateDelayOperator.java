5668544845387ea39d930d567065f1b69b91ee40
Arka Bhuiyan <belmont66@duck.com>
2025-05-17 14:06:04 +0600

    demonstrated the usage of the "delayElements" operator

---
+++ src/main/java/org/ablonewolf/flux/operators/DemonstrateDelayOperator.java
@@ -0,0 +1,34 @@
+package org.ablonewolf.flux.operators;
+
+import org.ablonewolf.common.Util;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/**
+ * This class demonstrates the usage of Reactor's "delayElements" operator to delay the emission
+ * of elements in a reactive stream by a specified duration.
+ * <p>
+ * The functionality involves creating a Flux stream of integers, applying a delay to each
+ * emitted item, and logging the elements as they are processed using a custom subscriber.
+ * <p>
+ * Key components:
+ * - `Flux.range()`: Generates a range of integers to be processed.
+ * - `delayElements(Duration)`: Introduces a delay for each element in the sequence.
+ * - `log()`: Logs the events occurring within the Flux stream for debugging or monitoring purposes.
+ * - `Util.subscriber(String)`: Subscribes to the stream with a custom subscriber that logs received data.
+ * - `Util.sleepSeconds(Long)`: Halts the main thread to ensure the program waits for the asynchronous
+ * processing to complete.
+ */
+public class DemonstrateDelayOperator {
+
+	public static void main(String[] args) {
+
+		Flux.range(1, 10)
+				.log()
+				.delayElements(Duration.ofMillis(100))
+				.subscribe(Util.subscriber("Integer Subscriber"));
+
+		Util.sleepSeconds(12L);
+	}
+}
