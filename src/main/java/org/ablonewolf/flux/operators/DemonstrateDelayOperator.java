a55de137f61d6f2db81e2f75984f831356e718e9
Arka Bhuiyan <belmont66@duck.com>
2025-05-17 14:07:50 +0600

    added the missing line breakers in the "Javadoc" comment.

--- src/main/java/org/ablonewolf/flux/operators/DemonstrateDelayOperator.java
+++ src/main/java/org/ablonewolf/flux/operators/DemonstrateDelayOperator.java
@@ -12,13 +12,13 @@ import java.time.Duration;
  * The functionality involves creating a Flux stream of integers, applying a delay to each
  * emitted item, and logging the elements as they are processed using a custom subscriber.
  * <p>
- * Key components:
- * - `Flux.range()`: Generates a range of integers to be processed.
- * - `delayElements(Duration)`: Introduces a delay for each element in the sequence.
- * - `log()`: Logs the events occurring within the Flux stream for debugging or monitoring purposes.
- * - `Util.subscriber(String)`: Subscribes to the stream with a custom subscriber that logs received data.
+ * Key components:<br>
+ * - `Flux.range()`: Generates a range of integers to be processed.<br>
+ * - `delayElements(Duration)`: Introduces a delay for each element in the sequence.<br>
+ * - `log()`: Logs the events occurring within the Flux stream for debugging or monitoring purposes.<br>
+ * - `Util.subscriber(String)`: Subscribes to the stream with a custom subscriber that logs received data.<br>
  * - `Util.sleepSeconds(Long)`: Halts the main thread to ensure the program waits for the asynchronous
- * processing to complete.
+ * processing to complete.<br>
  */
 public class DemonstrateDelayOperator {
 
