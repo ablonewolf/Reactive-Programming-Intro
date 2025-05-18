1c86415be56bbd1471321bfcdb97d66086ae0a12
Arka Bhuiyan <belmont66@duck.com>
2025-05-19 03:11:01 +0600

    created a package named model along with DTOs named Customer & PurchaseOrder

---
+++ src/main/java/org/ablonewolf/model/Customer.java
@@ -0,0 +1,4 @@
+package org.ablonewolf.model;
+
+public record Customer(Integer id, String name) {
+}
---
+++ src/main/java/org/ablonewolf/model/PurchaseOrder.java
@@ -0,0 +1,4 @@
+package org.ablonewolf.model;
+
+public record PurchaseOrder(String productName, Integer price, Integer quantity) {
+}
