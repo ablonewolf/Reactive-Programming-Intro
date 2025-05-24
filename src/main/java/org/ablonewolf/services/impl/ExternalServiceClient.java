d2a60fff8d980407f25f9d6da6e3e31e77586b27
Arka Bhuiyan <belmont66@duck.com>
2025-05-25 05:56:42 +0600

    delegated the managing of product name to the BoundedElastic scheduler for processing the product name in a non-blocking fashion.

--- src/main/java/org/ablonewolf/services/impl/ExternalServiceClient.java
+++ src/main/java/org/ablonewolf/services/impl/ExternalServiceClient.java
@@ -3,32 +3,34 @@ package org.ablonewolf.services.impl;
 import org.ablonewolf.common.AbstractHttpClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 /**
  * an external service client to communicate with the external service and consume its APIs
  */
 public class ExternalServiceClient extends AbstractHttpClient {
 
-    public Mono<String> getProductName(Integer productId) {
-        return this.httpClient.get()
-                .uri(String.format("/demo01/product/%d", productId))
-                .responseContent()
-                .asString()
-                .next();
-    }
+	public Mono<String> getProductName(Integer productId) {
+		return this.httpClient.get()
+				.uri(String.format("/demo01/product/%d", productId))
+				.responseContent()
+				.asString()
+				.next()
+				.publishOn(Schedulers.boundedElastic());
+	}
 
-    public Flux<String> getStreamOfNames() {
-        return this.httpClient.get()
-                .uri("/demo02/name/stream")
-                .responseContent()
-                .asString();
-    }
+	public Flux<String> getStreamOfNames() {
+		return this.httpClient.get()
+				.uri("/demo02/name/stream")
+				.responseContent()
+				.asString();
+	}
 
-    public Flux<Integer> getPriceChanges() {
-        return this.httpClient.get()
-                .uri("/demo02/stock/stream")
-                .responseContent()
-                .asString()
-                .map(Integer::parseInt);
-    }
+	public Flux<Integer> getPriceChanges() {
+		return this.httpClient.get()
+				.uri("/demo02/stock/stream")
+				.responseContent()
+				.asString()
+				.map(Integer::parseInt);
+	}
 }
