79c366b63944e2a5d3991cea9c513166e99319cb
Arka Bhuiyan <belmont66@duck.com>
2025-05-29 05:39:27 +0600

    removed the code fragments that demonstrated the thread-safety of FluxSink from the DemonstrateFluxCreate class; additionally removed the log along with its package.

--- src/main/java/org/ablonewolf/flux/DemonstrateFluxCreate.java
+++ src/main/java/org/ablonewolf/flux/DemonstrateFluxCreate.java
@@ -2,32 +2,30 @@ package org.ablonewolf.flux;
 
 import org.ablonewolf.common.Util;
 import org.ablonewolf.flux.helper.CountryNameGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * Demonstrates the creation of a Flux stream using the `create` method.
- * The program emits a stream of country names, handles subscriptions to the Flux,
- * and generates country names until a specific condition is reached.
+ * The DemonstrateFluxCreate class showcases the creation and usage of a Flux
+ * using the {@code Flux.create()} method, which allows for programmatically emitting items via a {@code FluxSink}.
+ * This example demonstrates the integration of a custom {@code CountryNameGenerator} that generates
+ * and streams random country names to a subscriber.
  * <p>
- * The main purpose of the class is to showcase Flux creation with a custom implementation of the
- * Reactive Streams' `Consumer` for handling the emission of data points.
- * The example also demonstrates concurrent execution and subscription handling in a
- * Reactive Streams context using FluxSink.
+ * Features of this implementation include:<br>
+ * - Creating a Flux instance using the {@code Flux.create()} method.<br>
+ * - Using a {@code CountryNameGenerator} as a provider of country names, which emits items
+ * to the Flux until a specified condition is met (emission of the name "Canada").<br>
+ * - Subscribing to the Flux for consuming the stream of emitted country names using a
+ * custom subscriber obtained from the {@code Util.subscriber()} method.<br>
+ * - Introducing delay in the main thread execution using the {@code Util.sleepSeconds()} method to
+ * allow the Flux to emit items before program termination.
  * <p>
- * Key Features:
- * - Utilizes the `Util` class for utility methods like generating random data, providing a subscriber,
- * and managing thread sleep operations.
- * - Demonstrates concurrent operations using threads for generating additional data.
- * - Logs the number of country names processed during program execution.
+ * Key concepts covered:<br>
+ * - Manual emitting of data in a Flux using {@code FluxSink}.<br>
+ * - Reactive Streams programming with data producers and consumers.<br>
+ * - The role of backpressure in controlling the flow of data between a Flux publisher
+ * and its subscribers.
  */
 public class DemonstrateFluxCreate {
-
-	private static final Logger log = LoggerFactory.getLogger(DemonstrateFluxCreate.class);
 
 	public static void main(String[] args) {
 
@@ -38,23 +36,7 @@ public class DemonstrateFluxCreate {
 
 		countryNameGenerator.generate();
 
-		List<String> countryNames = new ArrayList<>();
-
-		countryFlux.subscribe(countryNames::add);
-
-		Runnable runnable = () -> {
-			for (int i = 0; i < 1000; i++) {
-				countryNameGenerator.generateSingleName();
-			}
-		};
-
-		for (int i = 0; i < 10; i++) {
-			Thread.ofPlatform().start(runnable);
-		}
-
-		Util.sleepSeconds(5L);
-
-		log.info("Country name size: {}", countryNames.size());
+		Util.sleepSeconds(2L);
 
 	}
 }
