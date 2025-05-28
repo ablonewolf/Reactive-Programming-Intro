4c1450a0732a9b049bbc8a6ab6873dc7e6472c66
Arka Bhuiyan <belmont66@duck.com>
2025-05-29 04:25:30 +0600

    refactored the logic of CountryNameGenerator to another class for a modular purpose, adjusted the changes in the DemonstrateFluxCreate class as well.

--- src/main/java/org/ablonewolf/flux/DemonstrateFluxCreate.java
+++ src/main/java/org/ablonewolf/flux/DemonstrateFluxCreate.java
@@ -1,14 +1,13 @@
 package org.ablonewolf.flux;
 
 import org.ablonewolf.common.Util;
+import org.ablonewolf.flux.helper.CountryNameGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 /**
  * Demonstrates the creation of a Flux stream using the `create` method.
@@ -21,8 +20,6 @@ import java.util.function.Consumer;
  * Reactive Streams context using FluxSink.
  * <p>
  * Key Features:
- * - Defines a custom implementation of `Consumer<FluxSink<String>>` called `CountryNameGenerator`
- * that emits country names to a `Flux` stream.
  * - Utilizes the `Util` class for utility methods like generating random data, providing a subscriber,
  * and managing thread sleep operations.
  * - Demonstrates concurrent operations using threads for generating additional data.
@@ -59,53 +56,5 @@ public class DemonstrateFluxCreate {
 
 		log.info("Country name size: {}", countryNames.size());
 
-	}
-
-	/**
-	 * This class is responsible for generating a stream of country names using a Reactive Streams paradigm.
-	 * It implements the Consumer interface to accept a FluxSink instance for emitting items to a Flux.
-	 */
-	private static class CountryNameGenerator implements Consumer<FluxSink<String>> {
-
-		private FluxSink<String> fluxSink;
-
-		@Override
-		public void accept(FluxSink<String> fluxSink) {
-			this.fluxSink = fluxSink;
-		}
-
-		/**
-		 * Generates a stream of random country names and emits them to the associated fluxSink.
-		 * The generation continues until the name "Canada" (case-insensitive) is generated,
-		 * at which point the fluxSink completes.
-		 * <p>
-		 * This method uses the `Util.getFaker()` utility to generate random country names.
-		 * Each generated country name is passed to the FluxSink instance via its `next` method.
-		 * Upon emitting "Canada", the `complete` method of the fluxSink is invoked to
-		 * signal the completion of the stream.
-		 */
-		public void generate() {
-			String countryName;
-			do {
-				countryName = Util.getFaker().country().name();
-				this.fluxSink.next(countryName);
-			} while (!countryName.equalsIgnoreCase("canada"));
-
-			this.fluxSink.complete();
-		}
-
-		/**
-		 * Generates a single country name using the `Util.getFaker()` utility and emits it
-		 * to the associated FluxSink.
-		 * The generated country name is sent via the `next` method
-		 * of the FluxSink.
-		 * <p>
-		 * This method is useful for emitting a single country name to a Flux stream created
-		 * using the Reactive Streams paradigm.
-		 */
-		public void generateSingleName() {
-			String countryName = Util.getFaker().country().name();
-			this.fluxSink.next(countryName);
-		}
 	}
 }
---
+++ src/main/java/org/ablonewolf/flux/helper/CountryNameGenerator.java
@@ -0,0 +1,57 @@
+package org.ablonewolf.flux.helper;
+
+import org.ablonewolf.common.Util;
+import reactor.core.publisher.FluxSink;
+
+import java.util.function.Consumer;
+
+/**
+ * The CountryNameGenerator class is responsible for generating and emitting random country names
+ * using a FluxSink as the downstream data consumer in a Reactive Streams setup.
+ * This generator can emit either a continuous stream of names culminating upon generating "Canada"
+ * or a single name depending on the invoked method.
+ * It integrates the {@code Util.getFaker()} utility for random country name generation.
+ */
+public class CountryNameGenerator implements Consumer<FluxSink<String>> {
+
+	private FluxSink<String> fluxSink;
+
+	@Override
+	public void accept(FluxSink<String> fluxSink) {
+		this.fluxSink = fluxSink;
+	}
+
+	/**
+	 * Generates a stream of random country names and emits them to the associated fluxSink.
+	 * The generation continues until the name "Canada" (case-insensitive) is generated,
+	 * at which point the fluxSink completes.
+	 * <p>
+	 * This method uses the `Util.getFaker()` utility to generate random country names.
+	 * Each generated country name is passed to the FluxSink instance via its `next` method.
+	 * Upon emitting "Canada", the `complete` method of the fluxSink is invoked to
+	 * signal the completion of the stream.
+	 */
+	public void generate() {
+		String countryName;
+		do {
+			countryName = Util.getFaker().country().name();
+			this.fluxSink.next(countryName);
+		} while (!countryName.equalsIgnoreCase("canada"));
+
+		this.fluxSink.complete();
+	}
+
+	/**
+	 * Generates a single country name using the `Util.getFaker()` utility and emits it
+	 * to the associated FluxSink.
+	 * The generated country name is sent via the `next` method
+	 * of the FluxSink.
+	 * <p>
+	 * This method is useful for emitting a single country name to a Flux stream created
+	 * using the Reactive Streams paradigm.
+	 */
+	public void generateSingleName() {
+		String countryName = Util.getFaker().country().name();
+		this.fluxSink.next(countryName);
+	}
+}
