a652dd4511fc978d578a857f077cc13ac9e1f6f5
Arka Bhuiyan <belmont66@duck.com>
2025-05-30 01:35:30 +0600

    added another utility method to make the main thread sleep by passing a Duration object.

--- src/main/java/org/ablonewolf/common/Util.java
+++ src/main/java/org/ablonewolf/common/Util.java
@@ -10,32 +10,46 @@ import java.time.Duration;
 
 public class Util {
 
-    /**
-     * using a Faker instance to generate random fake names for various items
-     */
-    @Getter
-    private static final Faker faker = Faker.instance();
-    private static final Logger logger = LoggerFactory.getLogger(Util.class);
+	/**
+	 * using a Faker instance to generate random fake names for various items
+	 */
+	@Getter
+	private static final Faker faker = Faker.instance();
+	private static final Logger logger = LoggerFactory.getLogger(Util.class);
 
-    /**
-     * method for returning a new instance of default subscriber
-     * @param name: the name of the subscriber assigned by the publisher
-     * @return default subscriber
-     */
-    public static <T> Subscriber<T> subscriber(String name) {
-        return new DefaultSubscriber<>(name);
-    }
+	/**
+	 * method for returning a new instance of default subscriber
+	 *
+	 * @param name: the name of the subscriber assigned by the publisher
+	 * @return default subscriber
+	 */
+	public static <T> Subscriber<T> subscriber(String name) {
+		return new DefaultSubscriber<>(name);
+	}
 
-    /**
-     * make the main thread sleep for a certain amount of time
-     * @param seconds passed to the method to make Thread sleep for that duration of time
-     */
-    public static void sleepSeconds(Long seconds) {
-        try {
-            Thread.sleep(Duration.ofSeconds(seconds));
-        } catch (InterruptedException e) {
-            logger.error("Thread was interrupted due to this reason: {}", e.getMessage());
-        }
-    }
+	/**
+	 * make the main thread sleep for a certain amount of time
+	 *
+	 * @param seconds passed to the method to make Thread sleep for that duration of time
+	 */
+	public static void sleepSeconds(Long seconds) {
+		try {
+			Thread.sleep(Duration.ofSeconds(seconds));
+		} catch (InterruptedException e) {
+			printThreadInterruptedMessage(e);
+		}
+	}
+
+	public static void sleep(Duration duration) {
+		try {
+			Thread.sleep(duration);
+		} catch (InterruptedException e) {
+			printThreadInterruptedMessage(e);
+		}
+	}
+
+	private static void printThreadInterruptedMessage(InterruptedException e) {
+		logger.error("Thread was interrupted due to this reason: {}", e.getMessage());
+	}
 
 }
