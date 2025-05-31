37d623057b43b9668986fcd4048971c09aaca108
Arka Bhuiyan <belmont66@duck.com>
2025-05-31 11:19:12 +0600

    created a utility class to generate number stream to be used for combining publishers.

---
+++ src/main/java/org/ablonewolf/publisherCombination/NumberGenerator.java
@@ -0,0 +1,35 @@
+package org.ablonewolf.publisherCombination;
+
+import org.slf4j.Logger;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/**
+ * Utility class for generating streams of integer numbers as reactive sequences.
+ * The class provides static methods to generate different types of number streams.
+ * This class is final and cannot be instantiated.
+ */
+public final class NumberGenerator {
+
+	public static Flux<Integer> getNumberStream(Logger log) {
+		return Flux.create(sink -> {
+					for (int i = 3; i <= 100; i++) {
+						sink.next(i);
+					}
+					sink.complete();
+				})
+				.cast(Integer.class)
+				.doOnSubscribe(subscription -> log.info("Subscribing to Large number stream"))
+				.delayElements(Duration.ofMillis(10));
+	}
+
+	public static Flux<Integer> getMiniNumberStream(Logger log) {
+		return Flux.just(1, 2)
+				.doOnSubscribe(subscription -> log.info("Subscribing to Mini number stream"))
+				.delayElements(Duration.ofMillis(10));
+	}
+
+	private NumberGenerator() {
+	}
+}
