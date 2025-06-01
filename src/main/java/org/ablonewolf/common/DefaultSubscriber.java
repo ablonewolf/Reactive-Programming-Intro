7571e8d061f98ccd55b74c0c6471191e98b5f025
Arka Bhuiyan <belmont66@duck.com>
2025-06-02 05:14:00 +0600

    Added checks for Flux or Mono with empty items to log the state properly in the DefaultSubscriber class.

--- src/main/java/org/ablonewolf/common/DefaultSubscriber.java
+++ src/main/java/org/ablonewolf/common/DefaultSubscriber.java
@@ -6,29 +6,46 @@ import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 public class DefaultSubscriber<T> implements Subscriber<T> {
 
-    public static final Logger logger = LoggerFactory.getLogger(DefaultSubscriber.class);
-    private final String name;
+	public static final Logger logger = LoggerFactory.getLogger(DefaultSubscriber.class);
+	private final String name;
 
-    @Override
-    public void onSubscribe(Subscription subscription) {
-        subscription.request(Long.MAX_VALUE);
-    }
+	private boolean receivedItem = false;
+	private boolean completed = false;
 
-    @Override
-    public void onNext(T item) {
-        logger.info("{} received item {}.", this.name, item);
-    }
+	@Override
+	public void onSubscribe(Subscription subscription) {
+		subscription.request(Long.MAX_VALUE);
+	}
 
-    @Override
-    public void onError(Throwable throwable) {
-        logger.error("{} faced an error, error message is: '{}'.", this.name, throwable.getMessage());
-    }
+	@Override
+	public void onNext(T item) {
+		receivedItem = true;
 
-    @Override
-    public void onComplete() {
-        logger.info("{} completed receiving requests.", this.name);
-    }
+		if (item instanceof List && ((List<?>) item).isEmpty()) {
+			logger.info("{} received an empty list.", this.name);
+		} else {
+			logger.info("{} received item: {}.", this.name, item);
+		}
+	}
+
+	@Override
+	public void onError(Throwable throwable) {
+		receivedItem = true;
+		logger.error("{} faced an error. Error message: '{}'", this.name, throwable.getMessage(), throwable);
+	}
+
+	@Override
+	public void onComplete() {
+		if (!receivedItem && !completed) {
+			logger.info("{} did not receive any item.", this.name);
+		} else {
+			logger.info("{} completed receiving requests.", this.name);
+		}
+		completed = true;
+	}
 }
